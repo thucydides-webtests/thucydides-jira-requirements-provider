8a0ddbae3c0065f3b73ca0315cc92d55bf437f01
John Ferguson Smart <john.smart@wakaleo.com>
2013-08-29 00:06:35 +1000

    Cater for Yesterday in date format

--- src/main/java/net/thucydides/plugins/jira/adaptors/ZephyrDateParser.java
+++ src/main/java/net/thucydides/plugins/jira/adaptors/ZephyrDateParser.java
@@ -6,15 +6,19 @@ import org.joda.time.format.DateTimeFormat;
 public class ZephyrDateParser {
 
     private final DateTime today;
+    private final DateTime yesterday;
 
     public ZephyrDateParser(DateTime today) {
         this.today = today;
+        this.yesterday = today.minusDays(1);
     }
 
     public DateTime parse(String date) {
 
         if (date.contains("Today")) {
             date = date.replace("Today", today.toString(DateTimeFormat.forPattern("d/MMM/yy")));
+        } else if (date.contains("Yesterday")) {
+            date = date.replace("Yesterday", yesterday.toString(DateTimeFormat.forPattern("d/MMM/yy")));
         }
         return DateTime.parse(date, DateTimeFormat.forPattern("d/MMM/yy hh:mm a"));
     }
