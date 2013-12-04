73bcf5680fcb31959986d3f9aa6f73d9222724b7
John Smart <john.smart@wakaleo.com>
2013-12-04 23:17:13 +1100

    Updating requirements

---
+++ src/test/groovy/net/thucydides/plugins/jira/WhenReadingRequirementsFromJira.groovy
@@ -0,0 +1,38 @@
+package net.thucydides.plugins.jira
+
+import net.thucydides.core.model.TestOutcome
+import net.thucydides.core.model.TestTag
+import net.thucydides.core.util.MockEnvironmentVariables
+import net.thucydides.plugins.jira.requirements.JIRARequirementsProvider
+import net.thucydides.plugins.jira.service.JIRAConfiguration
+import net.thucydides.plugins.jira.service.SystemPropertiesJIRAConfiguration
+import spock.lang.Specification
+
+class WhenReadingRequirementsFromJira extends Specification {
+
+
+    JIRAConfiguration configuration
+    def environmentVariables = new MockEnvironmentVariables()
+    def requirementsProvider
+
+    def setup() {
+        environmentVariables.setProperty('jira.url','https://wakaleo.atlassian.net')
+        environmentVariables.setProperty('jira.username','bruce')
+        environmentVariables.setProperty('jira.password','batm0bile')
+        environmentVariables.setProperty('jira.project','TRAD')
+
+        configuration = new SystemPropertiesJIRAConfiguration(environmentVariables)
+        requirementsProvider = new JIRARequirementsProvider(configuration)
+    }
+
+    def "'Requirements can be loaded from the Epic/Story JIRA card structure"() {
+        given:
+            def requirementsProvider = new JIRARequirementsProvider(configuration)
+        when:
+            def requirements = requirementsProvider.getRequirements();
+        then:
+            requirements.size() > 0
+    }
+
+
+}
